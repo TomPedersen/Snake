635394a6ad14ba0de3dc0a7eef67c556c9c77c9c
TomPedersen <Knives89@gmail.com>
2015-08-26 13:51:07 +1200

    created game canvas and score box

--- Snake/Form1.Designer.cs
+++ Snake/Form1.Designer.cs
@@ -28,34 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.GameWorld = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.GameWorld)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // GameWorld
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(529, 459);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.GameWorld.Location = new System.Drawing.Point(13, 13);
+            this.GameWorld.Name = "GameWorld";
+            this.GameWorld.Size = new System.Drawing.Size(530, 460);
+            this.GameWorld.TabIndex = 0;
+            this.GameWorld.TabStop = false;
+            this.GameWorld.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(575, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Score:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelScore.Location = new System.Drawing.Point(704, 13);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(0, 37);
+            this.labelScore.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(740, 484);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(818, 484);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GameWorld);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GameWorld)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox GameWorld;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelScore;
     }
 }
 
--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -16,5 +16,15 @@ namespace Snake
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
