582b5255abff339695173e0a4b44833d4be18685
TomPedersen <Knives89@gmail.com>
2015-08-26 15:15:54 +1200

    created method for randomly-placed snake food on game canvas

--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -7,14 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Snake.Properties;
 
 namespace Snake
 {
     public partial class Form1 : Form
     {
+        private List<SnakeFood>Snake = new List<SnakeFood>();
+        private SnakeFood food = new SnakeFood();
+
         public Form1()
         {
             InitializeComponent();
+
+            //Set settings to default
+            new GameSettings();
+
+            //Set game speed and start timer
+            gameTimer.Interval = 1000/GameSettings.Speed;
+            gameTimer.Tick += UpdateScreen;
+            gameTimer.Start();
+
+            //Start new game
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            new GameSettings();
+
+            //create a new snake object
+            Snake.Clear();
+            SnakeFood head = new SnakeFood();
+            head.XAxis = 10;
+            head.YAxis = 5;
+            Snake.Add(head);
+
+
+            labelScore.Text = GameSettings.Score.ToString();
+            GenerateFood();
+        }
+
+        //Create randomly-placed food item in game world
+        private void GenerateFood()
+        {
+            int maxXPos = GameWorld.Size.Width/GameSettings.Width;
+            int maxYPos = GameWorld.Size.Height/GameSettings.Height;
+
+            Random random = new Random();
+            food = new SnakeFood();
+            food.XAxis = random.Next(0, maxXPos);
+            food.YAxis = random.Next(0, maxYPos);
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -23,6 +66,11 @@ namespace Snake
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
