f6931593bef206b94bee0f57d260d53975dff69c
TomPedersen <Knives89@gmail.com>
2015-08-26 13:57:29 +1200

    created SnakeFood class constructor. This will be the thing the snake eats to grow bigger

---
+++ Snake/SnakeFood.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class SnakeFood
+    {
+        public int XAxis { get; set; }
+        public int YAxis { get; set; }
+
+        public SnakeFood()
+        {
+            XAxis = 0;
+            YAxis = 0;
+        }
+    }
+}
